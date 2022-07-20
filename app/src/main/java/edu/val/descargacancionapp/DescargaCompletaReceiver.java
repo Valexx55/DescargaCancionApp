604dd3435e010df85cd4abdc8b9f31d533758e93
valexx55 <valexx55@gmail.com>
2022-07-20 19:13:24 +0200

    File Provider

--- app/src/main/java/edu/val/descargacancionapp/DescargaCompletaReceiver.java
+++ app/src/main/java/edu/val/descargacancionapp/DescargaCompletaReceiver.java
@@ -1,16 +1,71 @@
 package edu.val.descargacancionapp;
 
+import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
 
 //esta clase es la que va a escuchar el fin de la descarga del fichero
 public class DescargaCompletaReceiver extends BroadcastReceiver {
 
+
+    private Context context; //para saber desde qué actividad se ha lanzado la descarga
+    private long id_descarga; //así voy a saber cuál es el id de la descarga
+
+    public DescargaCompletaReceiver()
+    {
+
+    }
+
+    public DescargaCompletaReceiver (Context context)
+    {
+        this.context = context;//así, luego podré "meter mano" a la actividad una vez acabada la descarga
+    }
+
+    public void setId_descarga(long id_descarga) {
+        this.id_descarga = id_descarga;
+    }
+
     @Override //este método, se invoca por el sistema (recibe un callback) al completar la descarga que está "escuchando"
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Log.d("ETIQUETA_LOG", "descarga finalizada...");
+
+        //para acceder al servicio de descargar, necesito hacerlo desde el contexto
+        //uso, el context, creado en el constructor (que referencia a la pantalla inicial)
+        //para poder llamarlo
+        //obtengo el servicio de descargas
+        DownloadManager downloadManager = (DownloadManager) this.context.getSystemService(Context.DOWNLOAD_SERVICE);
+        //preparo la consulta
+        DownloadManager.Query query = new  DownloadManager.Query(); //vamos a consultar cuál ha sido el resultado de la descarga
+        query.setFilterById(this.id_descarga);//vamos a poder conusltar cómo ha ido nuestra descarga. En realidad, el DownloadMananger, ofrece una especie de ContentProvider
+        //hago la consulta
+        Cursor cursor = downloadManager.query(query); //dame los datos de la descarga con este id
+        cursor.moveToFirst();//avanzo el cursor al primer registro leido
+        int num_columno_estado = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+        int estado_descarga = cursor.getInt(num_columno_estado);
+
+        MainActivity mainActivity = (MainActivity) this.context;
+        switch (estado_descarga)
+        {
+            case DownloadManager.STATUS_SUCCESSFUL:
+                Log.d("ETIQUETA_LOG", "La descarga con ID " + this.id_descarga + " Acabó bien");
+                //TODO actualizar la Actividad desde donde llamamos
+                mainActivity.actualizarVentanaTrasDescarga(true);
+                break;
+
+            case DownloadManager.STATUS_FAILED:
+                Log.d("ETIQUETA_LOG", "La descarga con ID " + this.id_descarga + " Acabó mal");
+                //TODO actualizar la Actividad desde donde llamamos
+                mainActivity.actualizarVentanaTrasDescarga(false);
+
+                break;
+        }
+
+       //TODO hay que "desenchufar" este receiver - hacerlo que deje de escuchar.-.
     }
+
+
 }
--- app/src/main/java/edu/val/descargacancionapp/MainActivity.java
+++ app/src/main/java/edu/val/descargacancionapp/MainActivity.java
@@ -2,15 +2,25 @@ package edu.val.descargacancionapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,15 +43,20 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;//un dialog emergente que informa de la descarga
     private static final String URL_CANCION_COCIDITO = "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/a7/79/0b/mzm.qjnerkzx.aac.p.m4a";
     private boolean permiso_escritura_concedido;
+    private String ruta_fichero_descarga;
+    private File fichero_destino;
+    private Uri uri_descarga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.permiso_escritura_concedido = false;
         String[] array_permisos = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(array_permisos, 343);
+        } else {
+            //si tiene version inferior a Android 6, cuenta con los permisos
+            this.permiso_escritura_concedido = true;
         }
     }
 
@@ -51,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
         if (grantResults[0]== PackageManager.PERMISSION_GRANTED)
         {
             this.permiso_escritura_concedido = true;
+        } else {
+            this.permiso_escritura_concedido = false;
         }
     }
 
@@ -77,11 +94,58 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private DownloadManager.Request generarPeticionDescarga (String url)
+    {
+        DownloadManager.Request request = null;
+        Uri ruta_uri_recurso = null;
+        //TODO vamos a construir la petición para el Gestor de descargas
+            //QUÉ QUIERO DESCARGARME
+
+            //DÓNDE ESTÁ
+            //TIPO DE ARCHIVO
+
+            ruta_uri_recurso = Uri.parse(url);//convierto para que el string sea una dirección valida (tildes, espacios, caracteres especiales)
+            request = new DownloadManager.Request(ruta_uri_recurso);
+            request.setDescription("MÚSICA");
+            request.setTitle("Descarga muestra mp3");
+            request.setMimeType("audio/mp3"); //"Tipo de archivo que descargo "la extensión"
+
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);//hace visible la descarga al finalizar permance
+
+        //DÓNDE ME LO VA A GUADAR - este area de memoria es el directorio privado asociado a mi app
+            this.ruta_fichero_descarga = getExternalFilesDir(null).getPath()+"/cancion.mp3";
+            this.fichero_destino = new File(this.ruta_fichero_descarga);
+            this.uri_descarga = Uri.fromFile(this.fichero_destino);
+
+            request.setDestinationUri(uri_descarga);
+
+
+        return request;
+    }
+
     //en este método, lanzamos el proceso de descargar
     private void descargarFichero (String url_cancion)
     {
         dibujarProgressDialogo();
+        DescargaCompletaReceiver receiver = null;
+        IntentFilter filter = null;
 
+        //ESTOY PROGRAMANDO QUE EL RECEIVER DescargaCompletaReceiver
+        //ESTÉ "ESCUCHANDO" el evento QUE DIGA QUE SE HA TERMINADO LA DESCARGA
+        receiver = new DescargaCompletaReceiver(this);
+        filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(receiver, filter);//asociamos el receptor a ese mensaje (que será emitido por el DownloadManager)
+
+        //"la request sería como el PEDIDO " al DownloadMANAGER
+        DownloadManager.Request request = null; //este objeto es el que tiene toda la información sobre la descarga
+        request = generarPeticionDescarga(url_cancion);
+
+        //PIDO QUE DESCARGUE
+        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        long id_descarga = downloadManager.enqueue(request);//"encolar" - descarga esto!
+        receiver.setId_descarga(id_descarga);//al pedir al download que descarge, me da el ID de la descarga - ticket
+
+        /*
         Timer timer = new Timer();
 
          timer.schedule(new TimerTask() {
@@ -90,9 +154,62 @@ public class MainActivity extends AppCompatActivity {
                  MainActivity.this.progressDialog.dismiss();
              }
          }, 2000);
-
+        */
     }
 
     public void reproducirVisible(View view) {
+
+        try {
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            //TODO MAÑANA FILE PROVIDER y RERPDUCIR EL ARCHIVO
+
+            //En vez de usar la uri privada, vamos a usar una uri del file provider
+            //Aquí pasamos de una uri privada a una uri pública
+            Uri uri_publica = FileProvider.getUriForFile(this, "edu.val.descargacancionapp.fileprovider", fichero_destino);
+            Log.d("ETIQUETA_LOG", "uri_publica = " + uri_publica);
+            Log.d("ETIQUETA_LOG", "uri_privada = " + uri_descarga);
+
+            intent.setData(uri_publica);
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//así permitmos a la app externa, leer de mi ruta aunque no tenga permisos de linux
+
+            //intent.setData(uri_descarga);
+            //intent.setType("audio/*");
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                Log.d("ETIQUETA_LOG", "Al menos una app puede abrir un mp3");
+                startActivity(intent);
+            } else {
+                Log.d("ETIQUETA_LOG", "NINGUNA APP PUEDE ABRIR un mp3");
+            }
+
+
+
+        } catch (Exception e)
+        {
+            Log.e("ETIQUETA_LOG", "FALLO AL REPRODUCIR EL ARCHIVO", e);
+        }
+
+
+    }
+
+    public void actualizarVentanaTrasDescarga(boolean descarga_ok) {
+
+        this.progressDialog.dismiss();//oculto el diálogo de descarga
+
+        if (descarga_ok)
+        {
+            //le permito reproducir la canción descargada
+            Log.d("ETIQUETA_LOG", "descarga finalizada OK");
+            ImageButton imageView = findViewById(R.id.img_repoducir);
+            imageView.setBackgroundColor(getResources().getColor(R.color.purple_500));
+            imageView.setClickable(true); //una vez que descargue todo bien, cambio el color del botón y lo hago que se pueda dar click
+        } else {
+
+            Log.d("ETIQUETA_LOG", "descarga finalizada KO");
+            Toast toast = Toast.makeText(this, "Ha habido un problema en la descarga. Archivo no disponible", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
